e1db2c9c0b342ffc125f66a2ddcccc4bb8e8c2f9
Wesley <wesleystedman@gmail.com>
2020-09-01 15:39:22 -0700

    Add partial implementation of deck controller's create function. - Validation of inputs occurs - Deck is not added to the db - User is not redirected

--- controllers/decks.js
+++ controllers/decks.js
@@ -1,5 +1,6 @@
 const Deck = require('../models/deck');
 const Card = require('../models/card');
+const DECK_LINE_REGEX = /^\s*((\d+)\s*)?([A-Za-z '’,/-]+)(\s*\(([A-Za-z0-9]+)\)(\s*(\d+))?)?\s*$/;
 
 module.exports = {
     index,
@@ -26,8 +27,36 @@ function newDeck(req, res) {
 }
 
 function create(req, res) {
-    // TODO: implement
-    res.redirect('/');
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key];
+    }
+    console.log(req.body);
+    // process input - convert card names to cards, etc.
+    const deck = {};
+    deck.name = req.body.name;
+    deck.format = req.body.format.toLowerCase();
+    if (req.body.deckTile) {
+        validateCardLine(req.body.deckTile)
+            .then(cQPair => { deck.deckTile = cQPair })
+            .catch(err => { console.log(err) });
+    }
+    if (req.body.companion) {
+        validateCardLine(req.body.companion)
+            .then(cQPair => { deck.companion = cQPair })
+            .catch(err => { console.log(err) });
+    }
+    for (const prop of ['commandZone', 'mainDeck', 'sideboard', 'maybeboard']) {
+        if (req.body[prop]) {
+            deck[prop] = [];
+            const lines = req.body[prop].split('\\r\\n');
+            lines.forEach(line => {
+                validateCardLine(line)
+                    .then(cQPair => { deck[prop].push(cQPair) })
+                    .catch(err => { console.log(err) });
+            });
+        }
+    }
+    // add new deck to DB
 }
 
 function edit(req, res) {
@@ -43,4 +72,28 @@ function update(req, res) {
 function deleteDeck(req, res) {
     // TODO: implement
     res.redirect('/');
+}
+
+// accepts a MTGA import/export format string
+// returns a cardQuantitySchema-compatible object if the specified card exists
+// returns null if format is wrong or card does not exist
+function validateCardLine(line) {
+    const matches = line.match(DECK_LINE_REGEX);
+    if (!matches) return new Promise((resolve, reject) => { reject('Wrong line format') });
+
+    const filterQuery = {};
+    filterQuery.name = matches[3];
+    if (matches[5]) filterQuery.set = matches[5];
+    if (matches[7]) filterQuery.collector_number = matches[7];
+
+    return new Promise((resolve, reject) => {
+        Card.findOne(filterQuery, (err, card) => {
+            if (err) reject(err);
+            resolve({
+                quantity: matches[2] ? matches[2] : 1,
+                card: card._id
+            });
+        });
+
+    });
 }
