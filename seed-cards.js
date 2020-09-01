564c8cd8702f39912699a1096d762ff8385b5feb
Wesley <wesleystedman@gmail.com>
2020-08-31 19:40:44 -0700

    Add script to get card data from Scryfall API and put it in the DB.

---
+++ seed-cards.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+require('./config/database');
+const request = require('request-promise-native');
+const Card = require('./models/card');
+const SCRYFALL_REQUEST_URI = 'https://api.scryfall.com/cards/search?format=json&include_extras=false&include_multilingual=false&order=name&q=is%3Aarenaid&unique=prints'
+let pageCount = 0;
+
+function parseScryfall(uri) {
+    request(uri)
+        .then(results => {
+            const dataPage = JSON.parse(results);
+            dataPage.data.forEach(card => {
+                // rename id to scryfall_id and delete id, so that the virtual getter is not interfered with
+                card.scryfall_id = card.id;
+                delete card.id;
+                // findOneAndUpdate w/ upsert: true finds a card doc if it exists, and creates one if it doesn't, massively simplifying the logic here.
+                Card.findOneAndUpdate({ scryfall_id: `${card.scryfall_id}` }, card, { upsert: true }, (err, doc) => {
+                    if (err) console.log(err);
+                });
+            });
+            console.log(`Processed page ${++pageCount}`);
+            if (dataPage.has_more) {
+                setTimeout(() => {
+                    parseScryfall(dataPage.next_page);
+                }, 100);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+parseScryfall(SCRYFALL_REQUEST_URI);
